a65335f3478394c75bfc7ebce1f18b050ee22d1c
asadalfan <asadalfan87@gmail.com>
2018-10-02 14:34:24 +0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from '../app/server/server.component';
+import { PegawaiComponent } from './pegawai/pegawai.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    PegawaiComponent
   ],
   imports: [
     BrowserModule
